7bf4e18e8ba834224de1516bfe53dfac2450541c
ivancamilovelasquez <i.velasqueza@uniandes.edu.co>
2023-02-09 20:41:55 -0500

    Punto 5 a
    Punto5a

--- scripts/Codigo_taller1_parte4.R
+++ scripts/Codigo_taller1_parte4.R
@@ -14,15 +14,26 @@
 #  Objetivo del script: Este código corresponde al punto 5
 #_____________________________________________________________________________#
 
-# - Limpiar el environment y el panel
-
-rm(list = ls())
-cat("\014")
-
 # - Librerias
 
 library(pacman)
 p_load(rvest, tidyverse, ggplot2, robotstxt, psych)
 
+
+# - Fijar el Seed 
+set.seed(10101)
+
+
+# - Punto 5A
+
+# Crear una variable ID para la división de la base: train y test 
+
+GEIH$id <- 1:nrow(GEIH)
+
+
+#Dividir la base en 70%, 30%
+
+train <- GEIH %>% dplyr::sample_frac(0.70)
+test  <- dplyr::anti_join(GEIH, train, by = 'id')
 
 

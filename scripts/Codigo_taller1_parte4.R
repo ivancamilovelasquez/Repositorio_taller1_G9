eba4b5ae0cd52c2eb9bbe896fe40e7c8409e7b2e
ivancamilovelasquez <i.velasqueza@uniandes.edu.co>
2023-02-09 20:04:14 -0500

    Create Codigo_taller1_parte4.R

---
+++ scripts/Codigo_taller1_parte4.R
@@ -0,0 +1,28 @@
+#_____________________________________________________________________________#
+#                                                                             #
+#                      Problem Set 1: Predicting Income                       #
+#                                                                             #
+#_____________________________________________________________________________#
+
+#   Autores: - Jorge Rodríguez                                                  
+#            - Iván Velázquez  
+#            - Santiago Gonzalez
+#            - Maria Jose Colmenares
+#
+#  Fecha: 06/02/2023 
+#
+#  Objetivo del script: Este código corresponde al punto 5
+#_____________________________________________________________________________#
+
+# - Limpiar el environment y el panel
+
+rm(list = ls())
+cat("\014")
+
+# - Librerias
+
+library(pacman)
+p_load(rvest, tidyverse, ggplot2, robotstxt, psych)
+
+
+

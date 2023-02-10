55831a8d08fe273dfb123a5f808e5ebc2845d19f
Santiago Gonzalez Cepeda <123609985+santiagonzalezc@users.noreply.github.com>
2023-02-09 22:51:52 -0500

    Add files via upload

--- scripts/Codigo_taller1_parte4.R
+++ scripts/Codigo_taller1_parte4.R
@@ -9,31 +9,72 @@
 #            - Santiago Gonzalez
 #            - Maria Jose Colmenares
 #
-#  Fecha: 06/02/2023 
+#  Fecha: 08/02/2023 
 #
-#  Objetivo del script: Este código corresponde al punto 5
+#  Objetivo del script: Este código hace el punto 4 del taller, estudia
+#  las brechas salarias con respecto al género. 
 #_____________________________________________________________________________#
 
-# - Librerias
+# - Limpiar espacio de trabajo
 
-library(pacman)
-p_load(rvest, tidyverse, ggplot2, robotstxt, psych)
+rm(list = ls())
+
+# - Librerias y paquetes 
+
+require(pacman)
+p_load(rvest, tidyverse, ggplot2, robotstxt, psych, stargazer, boot)
+
+# - Brecha salarial de género
+# - a) Estimación y discusión de la brecha salarial incondicional:
+
+mod1 <- lm(GEIH$log_salario_m~GEIH$mujer)
+stargazer(mod1, type = "text")
+
+view(GEIH$oficio)
+
+# - b) ¿Salario igual para trabajos iguales?
+# - Estimación de una brecha de ingresos condicional incorporando variables de 
+# - control tales como características similares del trabajador y del puesto.
+# - i) Haciendo uso del Teorema FWL:
+
+mod2 <- lm(GEIH$log_salario_m~GEIH$mujer + GEIH$superior + GEIH$horas_trab_usual + GEIH$edad + GEIH$edad_2 + GEIH$informal)
+stargazer(mod2, type = "text")
 
 
-# - Fijar el Seed 
-set.seed(10101)
+# 1) paso 1
+GEIH<-GEIH %>% mutate(weightResidF=lm(log_salario_m ~ superior + horas_trab_usual + GEIH$edad + GEIH$edad_2 + GEIH$informal,GEIH)$residuals) #Residuals of weight~foreign 
+
+# 2) paso 2
+GEIH<-GEIH %>% mutate(mpgResidF=lm(mujer ~ superior + horas_trab_usual + GEIH$edad + GEIH$edad_2 + GEIH$informal,GEIH)$residuals) #Residuals of mpg~foreign 
+
+# 3) Regress the residuals from step 2 on the residuals from step 1
+
+reg2<-lm(weightResidF~mpgResidF,GEIH)
+stargazer(mod2,reg2,type="text",digits=7) # with stargazer we can visualize the coefficients next to each other
+
+# - ii) FWL con Bootstrap
 
 
-# - Punto 5A
+eta.fn<-function(data,index){
+  coef(lm(weightResidF~mpgResidF,GEIH, subset = index))
+}
 
-# Crear una variable ID para la división de la base: train y test 
+boot(GEIH, eta.fn, R = 1000)
 
-GEIH$id <- 1:nrow(GEIH)
+# - Gráfica de la brecha edad salario pronosticada con sus edades pico por género:
+
+ggplot() + 
+  geom_line(aes(x=GEIH$edad,y=predict(mod2, newdata = GEIH) , group=(GEIH$mujer)))
+  
+GEIH$prediccion <- predict(mod1, newdata = GEIH)
+GEIH[which.max(GEIH$prediccion),][5]
 
 
-#Dividir la base en 70%, 30%
 
-train <- GEIH %>% dplyr::sample_frac(0.70)
-test  <- dplyr::anti_join(GEIH, train, by = 'id')
+
+
+
+
+
 
 

5a172ac7f35cc45d124c6063c04dd1447f10f826
ivancamilovelasquez <i.velasqueza@uniandes.edu.co>
2023-02-11 01:29:46 -0500

    Punto 5b

--- scripts/Codigo_taller1_parte5.R
+++ scripts/Codigo_taller1_parte5.R
@@ -35,28 +35,76 @@ GEIH$id <- 1:nrow(GEIH)
 train <- GEIH %>% dplyr::sample_frac(0.70)
 test  <- dplyr::anti_join(GEIH, train, by = 'id')
 
+
+
 # - Punto 5B
 
+# Modelo base solo con la constante 
+mod1 <- lm(log_salario_m~1 , data = train)
+
 # Modelos punto 3
-mod1_train <- lm(log_salario_m~edad + edad_2, data = train)
+mod2 <- lm(log_salario_m~edad + edad_2, data = train)
 
 # Modelos punto 4
-mod2_train <- lm(log_salario_m~ mujer , data = train )
+mod3 <- lm(log_salario_m~ mujer , data = train )
 
-mod3_train <- lm(log_salario_m~ mujer + superior + horas_trab_usual + edad 
+mod4 <- lm(log_salario_m~ mujer + superior + horas_trab_usual + edad 
                  + edad_2 + informal, data = train)
 
 # Nuevos modelos 
 
-mod2_train <- lm(log_salario_m~ mujer , data = train )
+mod5 <- lm(log_salario_m~ mujer + superior + horas_trab_usual + edad 
+                 + edad_2 + informal + estrato + cabecera, data = train)
 
-mod3_train <- lm(log_salario_m~ mujer + superior + secundaria + horas_trab_usual 
-                 + exp_trab_actual + amo_casa + edad + edad_2 + informal, data = train)
+mod6 <- lm(log_salario_m~ mujer + superior + horas_trab_usual + edad 
+                 + edad_2 + informal + estrato + cabecera + media + superior , data = train)
 
-mod4_train <- lm(log_salario_m~ mujer + superior + secundaria + horas_trab_usual 
-                 + exp_trab_actual + amo_casa + edad + edad_2 + + oficio 
-                 + informal, data = train)
+mod7 <- lm(log_salario_m~ mujer + superior + horas_trab_usual + edad 
+                 + edad_2 + informal + estrato + cabecera + media + superior 
+                 + exp_trab_actual  , data = train)
 
-stargazer(mod1_train,mod2_train,mod3_train,mod4_train, type = "text")
+mod8 <- lm(log_salario_m~ mujer + superior + horas_trab_usual + edad 
+                 + edad_2 + informal + estrato + cabecera + media + superior 
+                 + exp_trab_actual + oficio , data = train)
+
+
+# Variables del 8 modelo 
+
+GEIH$edad_3 <- GEIH$edad^3
+GEIH$horas_trab_usual_2 <- GEIH$horas_trab_usual^2
+GEIH$exp_trab_actual_2 <- GEIH$exp_trab_actual^2
+
+#mod8_p5 <- lm(log_salario_m~ mujer + superior + horas_trab_usual + edad 
+                # + edad_2 + informal + estrato + cabecera + media + superior 
+                # + exp_trab_actual + oficio + edad_3 + horas_trab_usual_2 
+                 #+ exp_trab_actual_2 , data = train)
+
+#stargazer(mod0_p5, mod1_p5 , mod2_p5 , mod3_p5 , type = "text")
+#stargazer(mod4_p5, mod5_p5 , mod6_p5 , mod7_p5 , type = "text")       
+          
+
+#test$mod3 <- predict(mod3,newdata = test)
+#MSE_mod3  <- with(test,mean((log_salario_m-mod3)^2))
+
+
+MSE_mod <- numeric(5) 
+for (i in 1:5) {
+  modelo <- get(paste0("mod", i)) 
+  prediction <- predict(modelo,newdata = test)
+  test[[paste0("mod", i)]] <- prediction
+  MSE_mod[i] <- with(test, mean((log_salario_m-prediction)^2))
+}
+which.min(MSE_mod)
+
+# Dado que el mejor modelo es el 5,  guardamos la predicción y el verdadero valor 
+
+test$mejormodelo <- predict(mod5 ,newdata = test)
+test_subset <- subset(test, select = c("log_salario_m", "mejormodelo"))
+
+
+par(mfrow=c(1,2))
+plot(density(test_subset$log_salario_m), main="Distribución de valores verdaderos", col="red", xlab="Valor")
+plot(density(test_subset$mejormodelo), main="Distribución de valores predichos", col="blue", xlab="Valor")
+
 
 

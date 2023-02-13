e6e6680e26868c04a925e165180f4dc3974f6d3c
Jorge Rodríguez <123851986+jorgele054@users.noreply.github.com>
2023-02-12 22:22:17 -0500

    Ajuste grafica

--- scripts/Codigo_taller1_parte3.R
+++ scripts/Codigo_taller1_parte3.R
@@ -10,8 +10,8 @@
 #            - Maria Jose Colmenares
 #
 #  Fecha: 07/02/2023 
-
-p_load(stargazer,boot)
+library(pacman)
+p_load(stargazer,boot, bootstrap, ggplot2)
 
 # Punto 3: Age-wage profile
 
@@ -21,6 +21,9 @@ stargazer(reg,type="text", align = TRUE,
               no.space = TRUE, title = "Estimación Salario - Edad",
               out = "C:/Users/andre/OneDrive/Documentos/reg1.tex") 
 
+library(readxl)
+GEIH <- read_excel("C:/Users/jorge/Desktop/Problem Set 1/GEIHDATA.xlsx")
+
 # Bootstrap para los obtener errores estándar
 eta_fn<-function(data,index){
   coef(lm(log_salario_m~edad + edad_2, data = GEIH, subset = index)) 
@@ -29,26 +32,32 @@ eta_fn<-function(data,index){
 eta_fn(GEIH,1:nrow(GEIH))
 
 boot <- boot(GEIH, eta_fn, R = 1000)
+coef_boot <- boot$t0
+SE <- apply(boot$t,2,sd)
 
-# Peak age
-ggplot() + 
-  geom_line(aes(x=GEIH$edad,y=predict(reg, newdata = GEIH)))
+# Crear un dataframe con las x y las y
 
-GEIH$prediccion <- predict(reg, newdata = GEIH)
-GEIH[which.max(GEIH$prediccion),][5]
+x <- seq(18, 90, length.out = 100)
+y <- coef_boot[1] + coef_boot[2] * x + coef_boot[3] * x^2
+y_i <- (coef_boot[1]-1.96*SE[1]) + (coef_boot[2]-1.96*SE[2])*x + 
+       (coef_boot[3]-1.96*SE[3])*x^2
+y_s <- (coef_boot[1]+1.96*SE[1]) + (coef_boot[2]+1.96*SE[2])*x + 
+       (coef_boot[3]+1.96*SE[3])*x^2
 
-# Construir los intervalos de confianza
-b <- quantile(apply(boot$t,2,sd), c(0.025,0.975))
-GEIH$low <- GEIH$prediccion - b[1]
-GEIH$up <- GEIH$prediccion + b[2]
+df <- data.frame(x, y, y_i, y_s)
 
-# Gráfica del Peak Age
-library(ggplot2)
-p3 <- ggplot() +
-  geom_line(aes(x=GEIH$edad,y=predict(reg, newdata = GEIH))) +
-  geom_vline(xintercept = 43, color = "dark green") +
-  geom_errorbar(aes(x= GEIH$edad, ymin = GEIH$low, ymax = GEIH$up)) +
-  theme_minimal() +
-  labs(x = "Edad", y = "Logaritmo del Salario")
+# Graficar la función
+
+p3 <- ggplot(df, aes(x = x, y = y)) +
+  geom_line(aes(color = "Estimado"), size = 1) +
+  geom_line(aes(x = x, y = y_i, color = "Límite inferior"), linetype = "dotted", size = 1) +
+  geom_line(aes(x = x, y = y_s, color = "Límite superior"), linetype = "dotted", size = 1) +
+  scale_color_manual(name = "", values = c("Estimado" = "blue", "Límite inferior" = "red", "Límite superior" = "red")) +
+  labs(x = "Edad", y = "Log(Salario)") +
+  theme_classic() +
+  scale_x_continuous(limits = c(18, 90)) +
+  geom_vline(xintercept = 43, linetype = "dotted") +
+  theme(legend.position = "bottom")
+
 
 ggsave(p3, filename = "C:/Users/andre/OneDrive/Documentos/punto3.png", height = 5, width = 6)
